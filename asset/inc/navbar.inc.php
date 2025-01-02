a6abf9bb4176bae50a59787687d875628847a448
Gaelpem <pembelegael@gmail.com>
2025-01-02 15:30:13 +0100

    responsive

---
+++ asset/inc/navbar.inc.php
@@ -0,0 +1,21 @@
+<header>
+    <!-- Navigation principale -->
+    <nav class="navbar">
+        <!-- Lien vers Accueil -->
+        <ul class="nav-links link-home">
+            <li><a href="#accueil-menu" aria-label="Retour à l'accueil">Accueil</a></li>
+        </ul>
+        
+        <!-- Logo -->
+        <div class="logo">
+            <img src="asset/img/logo.svg" alt="Logo de mon site">
+        </div>
+
+        <!-- Liens de navigation -->
+        <ul class="nav-links">
+            <li><a href="#projet-menu" aria-label="Voir mes projets">Projets</a></li>
+            <li><a href="#projet-about" aria-label="À propos de moi">/ À propos</a></li>
+            <li><a href="#contact-menu" aria-label="Contactez-moi">/ Contact</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -12,29 +12,8 @@
 </head>
 
 <body>
-
-<header>
-    <!-- Navigation principale -->
-    <nav class="navbar">
-        <!-- Lien vers Accueil -->
-        <ul class="nav-links link-home">
-            <li><a href="#accueil-menu" aria-label="Retour à l'accueil">Accueil</a></li>
-        </ul>
-        
-        <!-- Logo -->
-        <div class="logo">
-            <img src="asset/img/logo.svg" alt="Logo de mon site">
-        </div>
-
-        <!-- Liens de navigation -->
-        <ul class="nav-links">
-            <li><a href="#projet-menu" aria-label="Voir mes projets">Projets</a></li>
-            <li><a href="#projet-about" aria-label="À propos de moi">/ À propos</a></li>
-            <li><a href="#contact-menu" aria-label="Contactez-moi">/ Contact</a></li>
-        </ul>
-    </nav>
-</header>
-
+ <!-- NAV-->
+<?php require_once 'asset/inc/navbar.inc.php' ?>
  <!-- Presentation-->
 
 <main>
@@ -42,13 +21,13 @@
 <section class="home">
     <div class="home-container">
     <p class="paragraphe1" id="accueil-menu">
-    <span class="salut">Salut,</span>
+    <span class="salut">Salut,</span><br>
     Je m’appelle Gaël Pembele, je suis un apprenti développeur, passionné par la technologie et le design, c'est pourquoi j'ai choisi de combiner ces deux univers.</p>
     <ul class="link-contact">
         <li><a href="#contact-menu">Contactez-moi</a></li>
-        <h2 class="titre-portfolio">PORTFOLIO&copy;</span></h2>
     </ul>
-    </div>
+    </div> 
+    <h2 class="titre-portfolio">PORTFOLIO&copy;</span></h2>
   </section>
 
 
@@ -60,13 +39,13 @@
 
    
     <article class="photos">
-    <h2 class="portfolio">Portfolio-Photos</h2>
+    <h2 class="portfolio"><a href="projet-photo.php">Portfolio Photos</a></h2>
         <div class="projet-photo"></div>
     </article>
 
     
     <article class="biographie">
-    <h2 class="portfolio">Biographie</h2>
+    <h2 class="portfolio"><a href="">Biographie</a></h2>
     <div class="projet-galle"></div>
     </article>
     
@@ -75,51 +54,39 @@
 
  <!--A propos-->
 
-<section  class="portfolio-section" >
-         
-         <h1 id="projet-about">[À-propos]<span class="number">(02)</span></h1>  
-         <div class="propos-container">
-             <article class="grid ">
-             <p class="paragraphe">Étudiant en première année de développement web à Digital Campus Paris, je me considère comme un  futur creative developer, passionné par le front-end et le design.</p>
-         
-             <p class="paragraphe">J’aime allier l’aspect artistique et technique du développement web tout en approfondissant le back-end pour devenir un développeur complet.</p>
-         
-             <p class="paragraphe">
-             En dehors du code, je m’intéresse à la fashion photographie, et la street photographie. Ce portfolio met en avant mes réalisations.
-             </p>
-             <div class="paragraphe">         
-        <h2 class ="competence">Compétences</h2>
-        <div class="skills-grid">
-            <div class="groupe">
-                 <h3 class="web">Web</h3>
-                  <p class="skills">
-                       html/css
-                       javascript
-                       php
-                       WordPress</p>
-             </div>
-              <div class="groupe">
-                 <h3 class="web">Design</h3>
-                    <p class="skills">
-                       illustrator 
-                       photoshop
-                       indesign
-                       figma</p>
-             </div>
-        </div>
-             </div>
-               <ul class="cv">
-                 <li><a href="">.Voir mon CV</a></li>
-               </ul>
-             </article>
-   </div>
+ <section class="portfolio-section">
+    <h1 id="projet-about">[À-propos]<span class="number">(02)</span></h1>
+    <div class="propos-container">
+        <article class="grid">
+            <p class="paragraphe">Étudiant en première année de développement web à Digital Campus Paris, je me considère comme un futur creative developer, passionné par le front-end et le design.</p>
+            <p class="paragraphe">J’aime allier l’aspect artistique et technique du développement web tout en approfondissant le back-end pour devenir un développeur complet.</p>
+            <p class="paragraphe">En dehors du code, je m’intéresse à la fashion photographie, et la street photographie. Ce portfolio met en avant mes réalisations.</p>
+            <div class="paragraphe">
+                <h2 class="competence">Compétences</h2>
+                <div class="skills-grid">
+                    <div class="groupe">
+                        <h3 class="web">Web</h3>
+                        <p class="skills">html/css<br>javascript<br>php<br>WordPress</p>
+                    </div>
+                    <div class="groupe">
+                        <h3 class="web">Design</h3>
+                        <p class="skills">illustrator<br>photoshop<br>indesign<br>figma</p>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+     <ul class="cv">
+                <li><a href="asset/cv/Gael_Pembele_CV.pdf" target="_blank">.Voir mon CV</a></li>
+      </ul>
 </section>
+
  <!--Contact-->
 
 <?php require "asset/inc/contact.inc.php" ?>
 
  <!--Menu-->
-<section class="portfolio-section">
+<section class="portfolio-section menu">
 <h1 class="menu-titre">[Men-u]<span class="number">(04)</span></h1> 
 <div class="menu-block">
       <ul class="links-block">
@@ -127,43 +94,16 @@
                     <li><a href="#projet-menu">[Proje-ts]<span class="numero">(02)</span></a></li>
                     <li><a href="#projet-about">[À-propos]<span class="numero">(03)</span></a></li>
                     <li><a href="#contact-menu">[Conta-ct]<span class="numero">(04)</span></a></li>
-               </ul>
+     </ul>
 </div>
 </section>
 
 
 </main>
 
+  <!--footer-->
+<?php  require_once 'asset/inc/footer.inc.php'?>
  <!--footer-->
-
-<footer>
-    <div class="social-lieu-copyright"> 
-         <ul class="social">
-            <li><a href=""><span class="grand-titre">Social</span></a></li>
-            <li><a href="https://www.instagram.com/whvshinngtonn/">instagram</a></li>
-            <li><a href="">Linkedin</a></li>
-            <li><a href="">Github</a></li>
-         </ul>
-          <div class="lieu">
-                 <p><span class="grand-titre">Lieu</span></p>
-                 <p>Paris,</p>
-                 <p>France</p>
-             </div>
-    
-        <div class="logo-blanc">
-        <svg id="a" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 179.832 126.3">  
-        <circle class="b" cx="97.632" cy="115" r="11.3"/> 
-        <path class="b" d="M82.132.7h-10.7c-1.1,0-2.1.9-2.1,2.1v11.9C57.231,3.3,38.932-.1,23.132,7.6,2.732,17.5-5.768,42.2,4.132,62.5c7.1,14.6,21.8,23.1,37,23.1,6,0,12.1-1.3,17.9-4.1,3.8-1.9,7.2-4.2,10.2-7.1v10.5c-.2,14.4-11.9,26-26.3,26s-25.1-10.6-26.2-24c-.1-1.3-1.1-2.3-2.4-2.3H4.332c-1.4,0-2.5,1.1-2.4,2.5,1.3,21.5,19.2,38.6,41,38.6,3.7,0,7.3-.5,10.9-1.5,3.4-.9,6.8-2.3,9.8-4.2,3-1.8,5.8-4,8.3-6.4s4.7-5.3,6.4-8.3,3.2-6.4,4.1-9.8c.7-2.6,1.3-5.3,1.3-8l.2-84.8c0-1.1-.9-2.1-2.1-2.1l.3.1ZM66.032,53.1c-2.3,6.6-7,12-13.4,15.1-13.1,6.4-28.8.9-35.2-12.2-6.3-13.1-.9-28.8,12.2-35.2,3.7-1.8,7.6-2.7,11.5-2.7,9.8,0,19.2,5.5,23.7,14.8h0c3.1,6.3,3.5,13.5,1.2,20.1v.1Z"/>
-  <path class="b" d="M138.731.1c-14,0-26.4,7.1-33.9,17.8V2.7c0-1.2-.9-2.1-2.1-2.1h-10.6c-1.2,0-2.1.9-2.1,2.1v86.2c0,1.2.9,2.1,2.1,2.1h10.6c1.2,0,2.1-.9,2.1-2.1v-24.5c7.4,10.8,19.8,17.8,33.9,17.8,22.7,0,41.1-18.4,41.1-41.1S161.432,0,138.731,0v.1ZM138.731,67.6c-14.5,0-26.3-11.8-26.3-26.3s11.8-26.3,26.3-26.3,26.3,11.8,26.3,26.3-11.8,26.3-26.3,26.3Z"/>
-        </svg>   
-    </div>
-
-          <div class="droit">
-          <p>Copyright © 2025</p>
-            <p>Tous droits réservés.</p>
-        </div>
-    </div>
-</footer>
 
 
 </body>

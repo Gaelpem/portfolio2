6761ba09967020715497dfa525fb1e0a3bb3e77f
Gaelpem <pembelegael@gmail.com>
2025-01-04 17:45:21 +0100

    responsive

--- asset/inc/contact.inc.php
+++ asset/inc/contact.inc.php
@@ -2,7 +2,7 @@
     <div class="contact-container">  
      <h2 id="contact-menu">[Conta-ct]<span class="number">(03)</span></h2> 
       <div class="contact-mini">
-    <p>Pour toutes questions, avis, opportunités de stage ou d’alternance, n’hésitez pas à me contacter. Je suis ouvert aux échanges et prêt à discuter avec vous.</p>
+       <p>Pour toutes questions, avis, opportunités de stage ou d’alternance, n’hésitez pas à me contacter. Je suis ouvert aux échanges et prêt à discuter avec vous.</p>
       <form action="" method="">
          <label for="nom">Nom</label>
          <input type="text" name="user_nom" id="nom" placeholder="Paul">
@@ -10,6 +10,7 @@
          <input type="text" name="user_email" id="email" placeholder="exemple@gmail.com">
          <button type="submit">Envoyez</button>
       </form>
+      </div> 
       <ul class="reseau">
         <li><a href="">pembelegael@gmail.com</a></li>
         <li><a href="">linkedin</a></li>
--- asset/inc/navbar.inc.php
+++ asset/inc/navbar.inc.php
@@ -22,14 +22,28 @@
     </div>
     </nav>
 
-    <div class="burger-menu">
+    <div class="burger-menu open">
     <ul class="nav-links link-home">
             <li><a href="#accueil-menu" aria-label="Retour à l'accueil">Accueil</a></li>
             <li><a href="#projet-menu" aria-label="Voir mes projets">Projets</a></li>
             <li><a href="#projet-about" aria-label="À propos de moi">À propos</a></li>
             <li><a href="#contact-menu" aria-label="Contactez-moi"> Contact</a></li>
         </ul>
-
+    
+    <div class="indicateur">
+        <p>Développé par Gaël Pembele</p>
+    </div>
         
     </div>
 </header>
+
+<script>
+    const burgerMenuButton = document.querySelector('.burger-menu-button')
+    const burgerMenuP = document.querySelector('.burger-menu-button p')
+    const burgerMenu = document.querySelector('.burger-menu')
+    
+
+    burgerMenuButton.onclick = function () {
+        burgerMenu.classList.toggle('open'); // Ajoute ou retire la classe "open"
+    };
+</script>
